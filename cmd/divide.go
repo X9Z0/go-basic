bcfcb299e289517e74243a38a154f409da04c445
X9Z0 <x9z0cofd@gmail.com>
2024-08-04 23:39:20 +0530

    added divide

---
+++ cmd/divide.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "errors"
+
+func Divide(a, b float64) (float64, error) {
+    if b == 0 {
+        return 0, errors.New("cannot divide by zero")
+    }
+    return a / b, nil
+}
+
